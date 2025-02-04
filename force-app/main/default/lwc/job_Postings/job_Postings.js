b171d9ad6e8a73370f9796e014ea7105f9a9943a
nachiketjagtap <nachiketjagtap9881@gmail.com>
2025-02-04 11:27:23 +0530

    Additional Notes section added..!!

--- force-app/main/default/lwc/job_Postings/job_Postings.js
+++ force-app/main/default/lwc/job_Postings/job_Postings.js
@@ -2,7 +2,7 @@ import { LightningElement, wire, track } from 'lwc';
 import getJobPostings from '@salesforce/apex/JobPostingController.getJobPostings';
 
 const columns = [
-    { label: 'Name', fieldName: 'Name', type: 'text' },
+    { label: 'Role', fieldName: 'Role__c', type: 'text' },
     { label: 'CTC/Budget', fieldName: 'CTC_Budget__c', type: 'number' },
     { label: 'Employment Type', fieldName: 'Employment_Type__c', type: 'text' },
     { label: 'Experience', fieldName: 'Experience__c', type: 'number' },
@@ -10,7 +10,9 @@ const columns = [
     { label: 'Key Responsibilities', fieldName: 'Key_Responsibilities__c', type: 'text' },
     { label: 'Location', fieldName: 'Location__c', type: 'text' },
     { label: 'Notice Period', fieldName: 'Notice_Period__c', type: 'number' },
-    { label: 'Role', fieldName: 'Role__c', type: 'text' }
+    { label: 'Additional Notes', fieldName: 'Additional_Notes__c', type: 'text' }
+
+   
 ];
 
 export default class JobPostings extends LightningElement {
